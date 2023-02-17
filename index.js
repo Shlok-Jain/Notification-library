9d1ae4d7e6074dae1c210f48b24019db28e2dcb0
Shlok <rubikscubeshlokjain@gmail.com>
2023-02-17 14:27:11 +0530

    add

--- index.js
+++ index.js
@@ -32,19 +32,27 @@ class NotificationJS {
         }
         switch(this.json.type){
             case 'success':
-                icon.innerHTML = '<img src="https://cdn.jsdelivr.net/gh/Shlok-Jain/Notification-library@latest/icons/success.png" class="Notification-library-image"/> '
+                icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#1bff00" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+              </svg> `
                 my_loader.classList.add("Notification-library-loader-success")
                 break
             case 'alert':
-                icon.innerHTML = '<img src="https://cdn.jsdelivr.net/gh/Shlok-Jain/Notification-library@latest/icons/alert.png" class="Notification-library-image"/> '
+                icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="yellow" class="bi bi-exclamation-octagon-fill" viewBox="0 0 16 16">
+                <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+              </svg>`
                 my_loader.classList.add("Notification-library-loader-alert")
                 break        
             case 'error':
-                icon.innerHTML = '<img src="https://cdn.jsdelivr.net/gh/Shlok-Jain/Notification-library@latest/icons/error.png" class="Notification-library-image"/> '
+                icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="red" class="bi bi-exclamation-octagon-fill" viewBox="0 0 16 16">
+                <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+              </svg>`
                 my_loader.classList.add("Notification-library-loader-error")
                 break
             case 'normal':
-                icon.innerHTML = '<img src="https://cdn.jsdelivr.net/gh/Shlok-Jain/Notification-library@latest/icons/normal.png" class="Notification-library-image"/> '
+                icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="orange" class="bi bi-bell" viewBox="0 0 16 16">
+                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+              </svg>`
                 my_loader.classList.add("Notification-library-loader-normal")
                 break
         }
